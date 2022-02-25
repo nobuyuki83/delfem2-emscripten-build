f715c6e53197495c3ab4ba7e2cf2839ff7e2445e
Nobuyuki <n.umetani@gmail.com>
2022-02-25 21:33:16 +0900

    ci

--- build_macos.sh
+++ build_macos.sh
@@ -3,62 +3,30 @@ echo "################################"
 echo "build examples_glfwnew"
 echo "################################"
 
-option="-o index.js -std=c++17 -I../external/delfem2/include -s USE_WEBGL2=1 -s USE_GLFW=3 -s WASM=1"
+option="-o index.js -std=c++17 -I../../external/delfem2/include -s USE_WEBGL2=1 -s USE_GLFW=3 -s WASM=1"
 
-cd 00_openwin
-pwd
-em++ ../external/delfem2/examples_newgl_glfw/00_openwin/main.cpp $option
-cd ../
+names=(
+	"00_openwin" 
+	"01_drawrect" 
+	"02_nav3d" 
+	"03_texture" 
+	"04_render2texture" 
+	"05_offscreenprojection"
+	"20_cad2d"
+	"30_cadmsh2d"
+	"40_femcloth"
+	"41_fem2d_poisson"
+	"42_fem2d_linearsolid")
 
-cd 01_drawrect
-pwd
-em++ ../external/delfem2/examples_newgl_glfw/01_drawrect/main.cpp $option
-cd ../
+for name in ${names[@]}
+do 
+	cd docs/$name
+	pwd
+	em++ ../../external/delfem2/examples_newgl_glfw/$name/main.cpp $option
+	cd ../../
+done
 
-cd 02_nav3d
-pwd
-em++ ../external/delfem2/examples_newgl_glfw/02_nav3d/main.cpp $option
-cd ../
-
-cd 03_texture
-pwd
-em++ ../external/delfem2/examples_newgl_glfw/03_texture/main.cpp $option
-cd ../
-
-cd 04_render2texture
-pwd
-em++ ../external/delfem2/examples_newgl_glfw/04_render2texture/main.cpp $option
-cd ../
-
-cd 05_offscreenprojection
-pwd
-em++ ../external/delfem2/examples_newgl_glfw/05_OffScreenProjection/main.cpp $option
-cd ../
-
-cd 20_cad2d
-pwd
-em++ ../external/delfem2/examples_newgl_glfw/20_cad2d/main.cpp $option
-cd ../
-
-cd 30_cadmsh2d
-pwd
-em++  ../external/delfem2/examples_newgl_glfw/30_cadmsh2d/main.cpp $option
-cd ../
-
-cd 40_femcloth
-pwd
-em++ ../external/delfem2/examples_newgl_glfw/40_femcloth/main.cpp $option
-cd ../
-
-cd 41_fem2d_poisson
-pwd
-em++ ../external/delfem2/examples_newgl_glfw/41_fem2d_poisson/main.cpp $option
-cd ../
-
-cd 42_fem2d_linearsolid
-pwd
-em++ ../external/delfem2/examples_newgl_glfw/42_fem2d_linearsolid/main.cpp $option
-cd ../
+exit 0
 
 
 # =====================================
